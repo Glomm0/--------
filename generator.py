b4acc04edd3cc129b49e5cdd6036b71e37eede99
Develka <tronix2013@mail.ru>
2023-06-02 20:49:21 +0800

    Новый генератор функций

--- generator.py
+++ generator.py
@@ -8,31 +8,244 @@ end = ")"
 def getSign():
     return random.randint(0, 1)
 
+def addCon(formula, n):
+    var = ["x" + str(i) for i in range(1, n+1)]
+    var1 = ["-x" + str(i) for i in range(1, n+1)]
+    var.extend(var1)
+    func = formula + ","
+    freq = dict.fromkeys(var, 0)
+    cons = formula.replace(",", " ").replace("(", " ").replace(")", " ").split(" ")
+
+    for c in cons:
+        if len(c) > 0:
+            vars = c.split("|")
+            for v in vars:
+                freq.update([(v, freq.get(v) + 1)])
+    
+    func += start
+        
+
+    k = 0
+    t = 0
+    s = []
+    for i in range(n*2):
+        s.append(random.randint(1, 2*n * (n-1) * (n-2)))
+
+    freq = sorted(freq.items(), key=lambda x:x[1])
+
+    freq = dict(freq)
+
+    t = list(freq.items())[0][1]
+
+    v1 = list(freq.items())[0][0]
+
+    func += v1 + "|"
+
+    t += 1
+
+    freq.update([(v1, t)])
+
+    exclude = []
+    
+    ex1, ex2 = 0, 0
+
+    
+
+    ex1 = var.index(v1)
+
+    if ex1 <= (n*2 - 1) / 2:
+        ex2 = ex1 + n
+    else:
+        ex2 = ex1 - n
+    
+
+    exclude.append(var[ex1])
+    exclude.append(var[ex2])
+
+    
+
+    e1 = s[k]
+    e2 = s[k+1]
+    k += 2
+
+    t_temp = t + 2
+    b = []
+
+    for key in freq.keys():
+        if key not in exclude and key not in exclude and freq.get(key) <= t:
+            b.append(key)
+
+    while len(b) == 0:
+        t_temp += 1
+        for key in freq.keys():
+            if key not in exclude and key not in exclude and freq.get(key) <= t:
+                b.append(key)
+
+    v2 = b[e1 % len(b)]
+
+    func += v2 + "|"
+
+    ex1 = var.index(v2)
+
+    if ex1 <= (n*2 - 1) / 2:
+        ex2 = ex1 + n
+    else:
+        ex2 = ex1 - n
+    
+
+    exclude.append(var[ex1])
+    exclude.append(var[ex2])
+
+    
+    t_temp = t + 2
+
+    b = []
+
+    for key in freq.keys():
+        if key not in exclude and key not in exclude and freq.get(key) <= t:
+            b.append(key)
+
+    while len(b) == 0:
+        t_temp += 1
+        for key in freq.keys():
+            if key not in exclude and key not in exclude and freq.get(key) <= t:
+                b.append(key)
+
+    v3 = b[e2 % len(b)]
+    func += v3 + end
+
+
+    return(func)
+
 
 def generateKNF(n):
     var = ["x" + str(i) for i in range(1, n+1)]
+    var1 = ["-x" + str(i) for i in range(1, n+1)]
+    var.extend(var1)
     func = ""
-    for i in range(len(var)):
-        pos = []
-        ind = set()
-        ind.add(i)
-        if getSign():
-            pos.append(var[i])
+    freq = dict.fromkeys(var, 0)
+
+
+    for c in range(n):
+        func += start
+        
+
+        k = 0
+        t = 0
+        s = []
+        for i in range(n*2):
+            s.append(random.randint(1, 2*n * (n-1) * (n-2)))
+
+        freq = sorted(freq.items(), key=lambda x:x[1])
+
+        freq = dict(freq)
+
+        t = list(freq.items())[0][1]
+
+        v1 = list(freq.items())[0][0]
+
+        func += v1 + "|"
+
+        t += 1
+
+        freq.update([(v1, t)])
+
+        exclude = []
+        
+        ex1, ex2 = 0, 0
+
+        
+
+        ex1 = var.index(v1)
+
+        if ex1 <= (n*2 - 1) / 2:
+            ex2 = ex1 + n
         else:
-            pos.append("-" + var[i])
+            ex2 = ex1 - n
+        
 
-        for p in range(2):
-            j = random.randint(0, n-1)
-            while(j in ind):
-                j = random.randint(0, n-1)
+        exclude.append(var[ex1])
+        exclude.append(var[ex2])
 
-            ind.add(j)
-            if getSign():
-                pos.append(var[j])
-            else:
-                pos.append("-" + var[j])
+        
 
-        func += start + pos[0] + "|" + pos[1] + "|" + pos[2] + end + ","
-    return func[:-1:]
+        e1 = s[k]
+        e2 = s[k+1]
+        k += 2
 
+        t_temp = t + 2
+        b = []
+
+        for key in freq.keys():
+            if key not in exclude and key not in exclude and freq.get(key) <= t:
+                b.append(key)
+
+        while len(b) == 0:
+            t_temp += 1
+            for key in freq.keys():
+                if key not in exclude and key not in exclude and freq.get(key) <= t:
+                    b.append(key)
+
+        v2 = b[e1 % len(b)]
+
+        func += v2 + "|"
+
+        ex1 = var.index(v2)
+
+        if ex1 <= (n*2 - 1) / 2:
+            ex2 = ex1 + n
+        else:
+            ex2 = ex1 - n
+        
+
+        exclude.append(var[ex1])
+        exclude.append(var[ex2])
+
+        
+        t_temp = t + 2
+
+        b = []
+
+        for key in freq.keys():
+            if key not in exclude and key not in exclude and freq.get(key) <= t:
+                b.append(key)
+
+        while len(b) == 0:
+            t_temp += 1
+            for key in freq.keys():
+                if key not in exclude and key not in exclude and freq.get(key) <= t:
+                    b.append(key)
+
+        v3 = b[e2 % len(b)]
+        func += v3 + end + ","
+
+
+    return(func[:-1:])
+
+    # for i in range(len(var)):
+    #     pos = []
+    #     ind = set()
+    #     ind.add(i)
+    #     if getSign():
+    #         pos.append(var[i])
+    #     else:
+    #         pos.append("-" + var[i])
+
+    #     for p in range(2):
+    #         j = random.randint(0, n-1)
+    #         while(j in ind):
+    #             j = random.randint(0, n-1)
+
+    #         ind.add(j)
+    #         if getSign():
+    #             pos.append(var[j])
+    #         else:
+    #             pos.append("-" + var[j])
+
+    #     func += start + pos[0] + "|" + pos[1] + "|" + pos[2] + end + ","
+    # return func[:-1:]
     
+
+f = "(x1|x8|-x5),(x2|x9|x4),(x3|x7|-x9),(x4|-x3|-x5),(x5|-x1|x9),(x6|x3|x4),(x7|x2|-x4),(x8|x7|-x2),(x9|-x5|x8),(x10|x5|x7)"
+addCon("(x1|x8|-x5),(x2|x9|x4),(x3|x7|-x9),(x4|-x3|-x5),(x5|-x1|x9),(x6|x3|x4),(x7|x2|-x4),(x8|x7|-x2),(x9|-x5|x8),(x10|x5|x7)", 10)
+# generateKNF(5)
--- main.py
+++ main.py
@@ -1,37 +1,75 @@
 import generator
 def checking(index,var_vector):
-    temp=[0 for i in range(max(index.keys()))] #значения каждого дизьюнкта по умолчанию 
+    temp=[0 for i in range(len(var_vector))] #значения каждого дизьюнкта по умолчанию 
+    # В индексе хранится n записей, а в векторе кол-во разрядов равно кол-ву скобок в функции
+    # Т.к. скобок может быть больше чем пременных, следующий цикл не работает
     for i,znach in enumerate(var_vector):
-        for j in index[i+1]:
+        for j in index[(i+1)]:
             if j>0:
                 temp[j]+=int(znach)
             else:
                 temp[j]+=int(znach)^1
     ans=list(map(lambda x:1 if x>0 else 0,temp))
-    print(ans)
+    # print(ans)
     return ans.count(1)
 
+def setIndex(func):
+    index={}
+    temp_data=func.split(',')
+    for i,skobka in enumerate(temp_data):
+        skobka=skobka[1:-1:]
+        for num in skobka.split("|"):
+            znach=int(num[num.index('x')+1:])
+            if(znach not in index.keys()):
+                index[znach]=[-1*i if num[0]=='-' else i]
+            else:
+                index[znach].append(-1*i if num[0]=='-' else i)
+    return index
 
-index={}#хранит информацию о том, в каких скобках есть та или иная переменная
+
+
 NUMBER_OF_VARIABLES=10
 example=generator.generateKNF(NUMBER_OF_VARIABLES) #Функция для проверки
 
+index = setIndex(example)
+# print("{:0>10}".format(bin(1)[2:]))
 
-temp_data=example.split(',')
-for i,skobka in enumerate(temp_data):
-    skobka=skobka[1:-1:]
-    for num in skobka.split("|"):
-        znach=int(num[num.index('x')+1:])
-        if(znach not in index.keys()):
-            index[znach]=[-1*i if num[0]=='-' else i]
-        else:
-            index[znach].append(-1*i if num[0]=='-' else i)
 #Тупой перебор       
-with open("output.txt","w") as fle:
-    fle.write("")
-for i in range(pow(2,NUMBER_OF_VARIABLES)):
-    #print("{:0>10}".format(bin(i)[2:]))#Подумать над работой с бинарными данными
-    #print (checking(index,"{:0>10}".format(bin(i)[2:])))
-    with open("output.txt","a") as fle:
-        fle.write(str(checking(index,"{:0>10}".format(bin(i)[2:])))+"\n")
+# with open("output.txt","w") as fle:
+#     fle.write("")
+
+count = []
+rate = []
+# for i,znach in enumerate(("{:0>" + f"{10}" + "}").format(bin(1)[2:])):
+#     print(index[i+1])
+
+#цикл, который достраивает формулу, добавляя скобки, пока она разрешима
+#проблема с методом checking
+while True:
+    n = len(example.split(","))
+    index = setIndex(example)#хранит информацию о том, в каких скобках есть та или иная переменная
+    for i in range(pow(2, n)):
+        # print("{:0>10}".format(bin(i)[2:]))#Подумать над работой с бинарными данными
+        # print (checking(index,"{:0>10}".format(bin(i)[2:])))
+        count.append(checking(index, ("{:0>" + f"{n}" + "}").format(bin(i)[2:])))
+        # print(("{:0>" + f"{n}" + "}"))
+        # with open("output.txt","a") as fle:
+        #     fle.write(str(checking(index,"{:0>10}".format(bin(i)[2:])))+"\n")
+
+
+    for c in count:
+        if c == NUMBER_OF_VARIABLES:
+            rate.append(1)
+        else:
+            rate.append(0)
+
+    if rate.count(1) > 0:
+        example = generator.addCon(example, NUMBER_OF_VARIABLES)
+    else:
+        break
+
+
+
+print(rate.count(1))
+print("Percent of true answers: ", rate.count(1) / len(rate))
 print(example)
