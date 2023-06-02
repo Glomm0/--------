6a7a36eaf77f55a42955202387853c26c9f95a94
Glomm0 <103648212+Glomm0@users.noreply.github.com>
2023-06-02 15:22:00 +0800

    add

--- main.py
+++ main.py
@@ -13,19 +13,25 @@ def checking(index,var_vector):
 
 
 index={}#хранит информацию о том, в каких скобках есть та или иная переменная
-NUMBER_OF_VARIABLES=5
+NUMBER_OF_VARIABLES=10
 example=generator.generateKNF(NUMBER_OF_VARIABLES) #Функция для проверки
-print(example)
+
+
 temp_data=example.split(',')
 for i,skobka in enumerate(temp_data):
     skobka=skobka[1:-1:]
     for num in skobka.split("|"):
-        znach=int(num[num.index('x')+1])
+        znach=int(num[num.index('x')+1:])
         if(znach not in index.keys()):
             index[znach]=[-1*i if num[0]=='-' else i]
         else:
             index[znach].append(-1*i if num[0]=='-' else i)
 #Тупой перебор       
+with open("output.txt","w") as fle:
+    fle.write("")
 for i in range(pow(2,NUMBER_OF_VARIABLES)):
-    print("{:0>5}".format(bin(i)[2:]))#Подумать над работой с бинарными данными
-    print (checking(index,"{:0>5}".format(bin(i)[2:])))
+    #print("{:0>10}".format(bin(i)[2:]))#Подумать над работой с бинарными данными
+    #print (checking(index,"{:0>10}".format(bin(i)[2:])))
+    with open("output.txt","a") as fle:
+        fle.write(str(checking(index,"{:0>10}".format(bin(i)[2:])))+"\n")
+print(example)
