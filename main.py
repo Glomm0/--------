d15c58ec9ab3b83d09d9c401544083d8140606da
Glomm0 <103648212+Glomm0@users.noreply.github.com>
2023-06-09 20:13:07 +0800

    program folder created

--- main.py
+++ main.py
@@ -75,7 +75,54 @@ def run():
     print("Total desuncts: ",n)
     
     Particle.best_swarm_position=[]
+def run2():
+    global results,NUMBER_OF_VARIABLES,NUMBER_OF_ITERATIONS,ex
+#Different means to generate functions
+
+    # ex=generator.generateRandomKNF(NUMBER_OF_VARIABLES)
+    # ex,max_true=generator.generate_full_ranodm_KNF(NUMBER_OF_VARIABLES)
+    # ex=generator.generate_without_checking(NUMBER_OF_VARIABLES)
+    # ex="(x1|-x8|x5),(x2|x3|x20),(x3|-x20|-x17),(x4|x11|x3),(x5|x1|-x19),(x6|x10|x14),(x7|x6|-x2),(x8|-x9|-x20),(x9|-x8|-x1),(x10|-x6|-x1),(x11|x1|x20),(x12|-x19|x9),(x13|-x6|-x4),(x14|x20|-x8),(x15|-x7|-x17),(x16|x10|-x2),(x17|x19|x11),(x18|x7|x12),(x19|-x14|x20),(x20|-x19|-x2),(-x3|-x10|-x5),(-x11|-x10|-x18),(-x12|-x16|-x14),(-x13|-x9|-x16)"
+    # print(ex)
+
+    index=generator.setIndex(ex)
+    n=len(ex.split(','))
+    particles=[Particle() for i in range(20)]
+    for i in particles: #Initializing 
+        temp=[ random.randint(0,1) for j in range(NUMBER_OF_VARIABLES)]
+        temp2=[ random.randint(0,1) for j in range(NUMBER_OF_VARIABLES)]
+        i.set_position(temp)
+        i.set_speed(temp2)
+        checking_res=checking(index,temp,n)
+        if checking_res>checking(index,i.best_position,n):
+            i.best_position=temp
+        if checking_res>checking(index,Particle.best_swarm_position,n):
+            Particle.best_swarm_position=temp
+
+    for o in range(NUMBER_OF_ITERATIONS):#looping till answer or to maximum number of iterations
+        for i in particles:
+            
+            i.move_binary()
+            checking_res=checking(index,i.position,n)
+            if checking_res>checking(index,i.best_position,n):
+                i.best_position=i.position
+            if checking_res>=checking(index,Particle.best_swarm_position,n):
+                Particle.best_swarm_position=i.position
+                
+                if checking_res==n:
+                    #print(Particle.best_swarm_position,checking(index,Particle.best_swarm_position,n))
+                    print(checking(index,Particle.best_swarm_position,n))
+                    print("Total desuncts: ",n)
+                    return 0
+            results.append([o,checking(index,i.position,n)])
+        for i in particles:
+            # i.change_speed()
+            i.change_speed_binary()
+        # results.append([o,checking(index,Particle.best_swarm_position,n)])
+    print("Maximum found true desuncts:",checking(index,Particle.best_swarm_position,n))
+    print("Total desuncts: ",n)
     
+    Particle.best_swarm_position=[]   
 
 
 
@@ -97,13 +144,22 @@ try:
                 Particle.a1=float(param_data[0][3:]) #Particle best position
                 Particle.a2=float(param_data[1][3:])  #Swarm best position
                 Particle.w=float(param_data[2][2:])
+            print("Parameters are:")
+            print("a1=",Particle.a1)
+            print("a2=",Particle.a2)
+            print("w=",Particle.w)
+            mode=input("Choose mode to run (1-probability, 2-binary)")
+
             for j in iters:
                 for i,ex in enumerate(data):
                 
                     results=[]
                     NUMBER_OF_VARIABLES=max(setIndex(ex).keys())
                     NUMBER_OF_ITERATIONS=j
-                    run()
+                    if mode=='1':
+                        run()
+                    else:
+                        run2()
                     x=[i[0] for i in results]
                     y=[i[1] for i in results]
                     plt.subplot((len(data))*100+1*10+i+1)
